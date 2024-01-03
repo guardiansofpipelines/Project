078f7cad57b41604208c143ab15468c61d78c0c4
EhtishamUlHassan <muhammadjalalkhan7658@gmail.com>
2024-01-03 12:53:19 +0500

    Updated random_forest.py by adding metrics and logging them i mlflow

--- src/random_forest.py
+++ src/random_forest.py
@@ -1,7 +1,7 @@
 import mlflow
 import mlflow.sklearn
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import sys
@@ -51,10 +51,18 @@ def train(X_train, X_test, y_train, y_test):
 
         # Evaluate the model
         mse = mean_squared_error(y_test, y_pred)
+        mae = mean_absolute_error(y_test, y_pred)
+        r2 = r2_score(y_test, y_pred)
 
         # Log model parameters and metrics using MLflow
         mlflow.log_params(model.get_params())
         mlflow.log_metric("mse", mse)
+        mlflow.log_metric("mae", mae)
+        mlflow.log_metric("r2", r2)
+
+        print("Mean Squared Error: ", mse)
+        print("Mean Absolute Error: ", mae)
+        print("R-squared: ", r2)
 
         # Save the model with MLflow
         mlflow.sklearn.log_model(model, "random_forest_model")
